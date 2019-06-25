a9b8f5db343edb5af7a40d1020a809d23b20c119
komuW <komuw05@gmail.com>
2019-06-25 22:19:59 +0300

    m

--- src/main/task.kt
+++ src/main/task.kt
@@ -55,43 +55,53 @@ class InMemoryBroker : BaseBroker() {
 @Serializable
 data class TaskArgs<T>(val args: List<T>)
 
-class Task(
-        val broker: BaseBroker,
-        val queueName: String,
-        val taskName: String,
+
+interface BaseTask {
+    val broker: BaseBroker
+    val queueName: String
+    val taskName: String
+    /**
+    [drainDuration] is the duration(in seconds) that a worker should wait
+    after getting a termination signal(SIGTERM, SIGQUIT etc).
+    during this duration, the worker does not consumer anymore tasks from the broker,
+    the worker will continue executing any tasks that it had already dequeued from the [broker]
+     */
+    val drainDuration: Float
+    val logLevel: String
+    val json: Json
+
+    fun run(vararg args: String): Any {
+        println("Task.run called with args: $args :: argsAsList: ${args.toList()}")
+        return 1
+    }
+
+    fun delay(vararg args: String): Unit {
+        println("Task.delay called with args: $args :: argsAsList: ${args.toList()}")
+        val jsonData = json.stringify(TaskArgs.serializer(StringSerializer), TaskArgs(args = args.toList()))
+        broker.enqueue(queueName = queueName, item = jsonData)
+    }
+}
+
+class ExampleTask(
+        override val broker: BaseBroker,
+        override val queueName: String,
+        override val taskName: String,
         /**
         [drainDuration] is the duration(in seconds) that a worker should wait
         after getting a termination signal(SIGTERM, SIGQUIT etc).
         during this duration, the worker does not consumer anymore tasks from the broker,
         the worker will continue executing any tasks that it had already dequeued from the [broker]
          */
-        private val drainDuration: Float = 10.0f,
-        private val logLevel: String = "INFO"
-) {
-
-    private val json = Json(JsonConfiguration.Stable)
-
-    fun aha(): Unit {
-        println(broker)
-        println(queueName)
-        println(taskName)
-        println(drainDuration)
-        println(logLevel)
-    }
-
-    fun delay(vararg args: String): Unit {
-        println("Task.delay called with args: $args and argsAsList: ${args.toList()}")
-        val jsonData = json.stringify(TaskArgs.serializer(StringSerializer), TaskArgs(args = args.toList()))
-        broker.enqueue(queueName = queueName, item = jsonData)
-    }
-}
+        override val drainDuration: Float = 10.0f,
+        override val logLevel: String = "INFO",
+        override val json: Json = Json(JsonConfiguration.Stable)
+) : BaseTask {}
 
 
 fun main(): Unit {
     val b = InMemoryBroker()
-    val tsk = Task(broker = b, queueName = "queueName", taskName = "MyTaskName")
+    val tsk = ExampleTask(broker = b, queueName = "queueName", taskName = "MyTaskName")
     println("main")
-    println(tsk.aha())
     println(tsk.taskName)
 
     // assuming the run method has a signature like;
