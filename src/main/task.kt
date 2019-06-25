4b2a14ecf64f6cec63e781b88790d0278c4e30b3
komuW <komuw05@gmail.com>
2019-06-25 22:59:35 +0300

    m

--- src/main/task.kt
+++ src/main/task.kt
@@ -3,7 +3,6 @@ package geti
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.internal.StringSerializer
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
 
 
 @Serializable
@@ -38,37 +37,3 @@ interface BaseTask {
     }
 }
 
-class ExampleTask(
-        override val broker: BaseBroker,
-        override val queueName: String,
-        override val taskName: String,
-        /**
-        [drainDuration] is the duration(in seconds) that a worker should wait
-        after getting a termination signal(SIGTERM, SIGQUIT etc).
-        during this duration, the worker does not consumer anymore tasks from the broker,
-        the worker will continue executing any tasks that it had already dequeued from the [broker]
-         */
-        override val drainDuration: Float = 10.0f,
-        override val logLevel: String = "INFO",
-        override val json: Json = Json(JsonConfiguration.Stable)
-) : BaseTask {
-    override fun run(vararg args: String): Any {
-        println("ExampleTask.run called with args: $args :: argsAsList: ${args.toList()}")
-        return args
-    }
-}
-
-
-fun main(): Unit {
-    val b = InMemoryBroker()
-    val tsk = ExampleTask(broker = b, queueName = "queueName", taskName = "MyTaskName")
-    println("main")
-    println(tsk.taskName)
-
-    // assuming the run method has a signature like;
-    // .run(log_id, age, name)
-    tsk.delay("qejq4j242", "90", "John")
-
-    tsk.delay("222", "22", "kili")
-    tsk.delay("333", "333", "mili")
-}
