de44c50e2fc66d63f9a0a76deff56e716b3709c4
komuw <komuw05@gmail.com>
2019-06-29 17:59:51 +0300

    f

--- src/main/Main.kt
+++ src/main/Main.kt
@@ -1,8 +1,14 @@
 package geti
 
+
 import com.github.kittinunf.fuel.Fuel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
+
+import  kotlinx.coroutines.delay
 
 class HttpTask : BaseTask {
     /**
@@ -10,14 +16,14 @@ class HttpTask : BaseTask {
      * then prints the response
      */
     override val broker = InMemoryBroker()
-    override val queueName = "queueName"
-    override val taskName = "MyTaskName"
+    override val queueName = "HttpTask"
+    override val taskName = "HttpTask"
     override val drainDuration: Float = 10.0f
     override val logLevel: String = "INFO"
     override val json: Json = Json(JsonConfiguration.Stable)
 
     override suspend fun run(args: HashMap<String, String>) {
-        println("ExampleTask.run called with args: $args ")
+        println("HttpTask.run called with args: $args ")
 
         val req = Fuel.get(args["url"] as String)
         val resp = req.responseString()
@@ -25,7 +31,23 @@ class HttpTask : BaseTask {
     }
 }
 
-fun main() {
+
+class PrintTask : BaseTask {
+    override val broker = InMemoryBroker()
+    override val queueName = "PrintTask"
+    override val taskName = "PrintTask"
+    override val drainDuration: Float = 10.0f
+    override val logLevel: String = "INFO"
+    override val json: Json = Json(JsonConfiguration.Stable)
+
+    override suspend fun run(args: HashMap<String, String>) {
+        println("PrintTask.run called with args: $args ")
+        delay(6000L)
+    }
+}
+
+
+fun main() = runBlocking<Unit> {
     val tsk = HttpTask()
     println("main")
     println(tsk.taskName)
@@ -35,11 +57,23 @@ fun main() {
     tsk.schedule(hashMapOf("url" to "https://httpbin.org/delay/3"))
     tsk.schedule(hashMapOf("url" to "https://httpbin.org/delay/7"))
 
-    for (i in 1..30) {
+    for (i in 1..8) {
         tsk.schedule(hashMapOf("url" to "https://httpbin.org/delay/2"))
     }
 
+    val pt = PrintTask()
+    for (i in 1..30) {
+        pt.schedule(hashMapOf("name" to "John"))
+    }
+
     // run workers
-    val worker = Worker(task = tsk, workerId = "workerId")
-    worker.consumeTasks()
+    launch(Dispatchers.Default) {
+        val worker1 = Worker(task = tsk, workerId = "workerId-1")
+        worker1.consumeTasks()
+    }
+
+    launch(Dispatchers.Default) {
+        val worker2 = Worker(task = pt, workerId = "workerId-2")
+        worker2.consumeTasks()
+    }
 }
