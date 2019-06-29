1659eff400a9be03a8e5e736d1ab36b3072d65fb
komuw <komuw05@gmail.com>
2019-06-29 17:40:20 +0300

    f

--- src/main/Worker.kt
+++ src/main/Worker.kt
@@ -12,21 +12,14 @@ class Worker(private val task: BaseTask, private val workerId: String) {
     fun consumeTasks() = runBlocking<Unit> {
         println("workerId: $workerId")
         while (true) {
-            val item: String = task.broker.dequeue(task.queueName)
-            println("dequeue item")
-            println(item)
-
-            val obj: TaskArgs = task.json.parse(TaskArgs.serializer(), item)
-            println()
-            println("parsed json obj")
-            println(obj)
-            println()
-
             launch(Dispatchers.Default) {
                 // TODO: add  CoroutineName("geti-worker-coroutine")
                 //the dispatcher use; is backed by a shared pool of threads on JVM.
                 // the max no of threads used is equal to the number of CPU cores.
-                task.run(obj.args)
+
+                val item: String = task.broker.dequeue(task.queueName)
+                val tArgs: TaskArgs = task.json.parse(TaskArgs.serializer(), item)
+                task.run(tArgs.args)
             }
 
         }
