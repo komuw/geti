d1340b3034248fc3cd9d0e452542295038ff68d9
komuw <komuw05@gmail.com>
2019-06-29 16:14:53 +0300

    m

--- src/main/Worker.kt
+++ src/main/Worker.kt
@@ -1,28 +1,31 @@
 package geti
 
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+
 class Worker(private val task: BaseTask, private val workerId: String) {
     /**
     docs
      */
 
-    fun consumeTasks() {
+    fun consumeTasks() = runBlocking<Unit> {
         println("workerId: $workerId")
-//        while (true) {
-//            val item: String = task.broker.dequeue(task.queueName)
-//            println("dequeue item")
-//            println(item)
-//        }
+        while (true) {
+            val item: String = task.broker.dequeue(task.queueName)
+            println("dequeue item")
+            println(item)
 
-        val item: String = task.broker.dequeue(task.queueName)
-        println("dequeue item")
-        println(item)
+            val obj: TaskArgs = task.json.parse(TaskArgs.serializer(), item)
+            println()
+            println("parsed json obj")
+            println(obj)
+            println()
 
-        val obj: TaskArgs = task.json.parse(TaskArgs.serializer(), item)
-        println()
-        println("parsed json obj")
-        println(obj)
-        println()
-
-        task.run(obj.args)
+            launch {
+                //            doWorld()
+                task.run(obj.args)
+            }
+        }
     }
 }
